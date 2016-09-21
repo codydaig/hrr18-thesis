722758607be1141fce33eef97999b9626d9e74f8
JonathonJulian <jfritz50@gmail.com>
2016-09-21 15:37:45 -0700

    change port for testing

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
-app.listen('8080')
+app.listen('80')
 app.use(express.static(path.join(__dirname, '../client')))
 
 
