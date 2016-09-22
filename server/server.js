a1e725f08233eecc03c1f819544e838067dba20f
JonathonJulian <jfritz50@gmail.com>
2016-09-21 18:13:40 -0700

    monogodb connection established

--- server/server.js
+++ server/server.js
@@ -9,3 +9,15 @@ app.use(express.static(path.join(__dirname, '../client')))
 app.all('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/index.html'))
 })
+
+const dbOptions = {
+  user: 'svc_dbadmin',
+  password: 'Fractal90'
+}
+
+
+mongoose.connect('mongodb://ds035806.mlab.com:35806/therapp', dbOptions)
+
+mongoose.connection.on('connected', function () {
+  console.log('Connection to MongoDb established!')
+})
