e4fa3230eda4bfcb2cb1e6e8caacf32b476b1209
JonathonJulian <jfritz50@gmail.com>
2016-09-21 18:17:33 -0700

    switching to fork from master source

--- server/server.js
+++ server/server.js
@@ -10,12 +10,13 @@ app.all('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/index.html'))
 })
 
+
+// MongoDb
+
 const dbOptions = {
   user: 'svc_dbadmin',
   password: 'Fractal90'
 }
-
-
 mongoose.connect('mongodb://ds035806.mlab.com:35806/therapp', dbOptions)
 
 mongoose.connection.on('connected', function () {
