4ce10fba283a254beac2982a293aa5805945d453
Jonathon Fritz <jfritz50@gmail.com>
2016-09-21 23:13:22 -0700

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { render } from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello, world!!</h1>  <h1>Hello, world!!</h1>,
-      document.getElementById('app')
-    )
-
-
-console.log('testing')
+  <div>
+  <h1>Hello, world!!</h1>  <h1>Hello, world!!</h1>
+  </div>,document.getElementById('app'))
