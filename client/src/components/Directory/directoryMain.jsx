b4c92863d636a655fce0aac0d8f3756919cfa2f1
JonathonJulian <jfritz50@gmail.com>
2016-09-27 15:00:21 -0700

    booking dialog with date and time picker added

--- client/src/components/Directory/directoryMain.jsx
+++ client/src/components/Directory/directoryMain.jsx
@@ -3,17 +3,24 @@ import axios from 'axios'
 import directoryListItem from './directoryListItem'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
+import Dialog from 'material-ui/Dialog';
+import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
+
+
 
 export default class directoryMain extends React.Component {
   constructor (props){
     super(props)
     this.state = {
         practitioners: [],
-        currentSelection: ''
+        currentSelection: '',
+        bookOpen: false
     }
     this.componentDidMount = this.componentDidMount.bind(this)
     this.componentWillUnmount = this.componentWillUnmount.bind(this)
     this.bookApointment = this.bookApointment.bind(this)
+    this. handleClose = this.handleClose.bind(this)
   }
   componentDidMount () {
     const that = this
@@ -30,18 +37,38 @@ export default class directoryMain extends React.Component {
 
   bookApointment(currentSelection){
      this.setState({
-       currentSelection: currentSelection
+       currentSelection: currentSelection,
+       bookOpen: true
      })    
    
   }
 
+   handleClose () {
+    this.setState({bookOpen: false});
+  };
+
   render () {
+
+ const actions = [
+      <FlatButton
+        label="Book"
+        primary={true}
+        keyboardFocused={true}
+        onTouchTap={this.handleClose}
+      />,
+    ];
+
+
 
     return (
       <div>
+      
+     
      {this.state.practitioners.map((practitioner)=>{
        
       return (
+
+        <div>
            <Card>  
              <CardHeader
               title={practitioner.user_metadata.firstName} 
@@ -55,6 +82,19 @@ export default class directoryMain extends React.Component {
               onTouchTap={this.bookApointment.bind(this, practitioner._id)  }
            />
             </Card>
+          
+          <Dialog
+          title="Choose a Date and Time for your Appointment" 
+          actions={actions}
+          modal={false}
+          open={this.state.bookOpen}
+          onRequestClose={this.handleClose}
+        >
+          Open a Date Picker dialog from within a dialog.
+          <DatePicker hintText="Date" />
+          <TimePicker  hintText="Time" />
+          </Dialog>
+          </div>
           )  
       })}
     </div>
